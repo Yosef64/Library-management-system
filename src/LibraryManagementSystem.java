974f2e4ab5b348488a8c2ca7aca8b1de384443fc
Yosef64 <yosefale65@gmail.com>
2024-04-27 19:12:26 +0300

    initial commit

---
+++ src/LibraryManagementSystem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class LibraryManagementSystem {
+    String libraryName;
+    String place;
+    private static ArrayList<Student> studentslist = new ArrayList<>();
+
+    LibraryManagementSystem(){
+        this.libraryName = "jan";
+        this.place = "bd";
+    }
+    public static ArrayList<Student> getStudentslist(){
+        return  studentslist;
+    }
+
+    public static void addStudent(Student std){
+        studentslist.add(std);
+    }
+
+}
